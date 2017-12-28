a0fde464547281f1f196e5ceb0426df1829a9d20
DiscordUser101 <34905162+DiscordUser101@users.noreply.github.com>
2017-12-27 23:05:17 -0600

    Create textbot.py

---
+++ textbot.py
@@ -0,0 +1,22 @@
+import asyncio
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+Client = discord.Client()
+bot_prefix = "---"
+client = commands.Bot(command_prefix = bot_prefix)
+
+@client.event
+async def on_ready():
+    print("Bot Online!")
+    print("Name: {}".format(client.user.name))
+    print("ID: {}".format(client.user.id))
+
+@client.event
+async def on_message(message):
+    if message.content.upper().startswith('!SAY'):
+        args = message.content.split(" ")
+        await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
+
+client.login(process.env.BOT_TOKEN)
